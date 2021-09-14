116b060da68c3878cbda86cb28a6a47a4635bf57
Trax-hub <78899389+Trax-hub@users.noreply.github.com>
2021-09-14 12:39:23 +0200

    add component_1

--- app/src/main/java/com/example/pts3/MainActivity.java
+++ app/src/main/java/com/example/pts3/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
