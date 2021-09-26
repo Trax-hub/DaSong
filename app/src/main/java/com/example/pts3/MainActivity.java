986392c0c4cb896e11f74d3711ac978d4660e539
Trax-hub <78899389+Trax-hub@users.noreply.github.com>
2021-09-26 16:09:20 +0200

    ajout onclick

--- app/src/main/java/com/example/pts3/MainActivity.java
+++ app/src/main/java/com/example/pts3/MainActivity.java
@@ -2,17 +2,36 @@ package com.example.pts3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button validateButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        validateButton = (Button) findViewById(R.id.validateButton);
+
+
+        validateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ACTION A REALISER
+            }
+        });
     }
 }
