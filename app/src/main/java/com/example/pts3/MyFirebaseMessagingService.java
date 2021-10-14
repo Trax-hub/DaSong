29a5397c32153eed9c3a82c02c1f6e9ba4e98907
jcholet <jonas.cholet@hotmail.fr>
2021-10-14 11:38:50 +0200

    NOTIFICATIONS

---
+++ app/src/main/java/com/example/pts3/MyFirebaseMessagingService.java
@@ -0,0 +1,38 @@
+package com.example.pts3;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        String myMessage = remoteMessage.getNotification().getBody();
+        Log.d("FirebaseMessage", "Vous venez de recevoir une notification" + myMessage);
+
+        String title = remoteMessage.getNotification().getTitle();
+        String text = remoteMessage.getNotification().getBody();
+        final String CHANNEL_ID = "HEADS_UP_NOTIFICATION";
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Heads up notification", NotificationManager.IMPORTANCE_HIGH);
+        getSystemService(NotificationManager.class).createNotificationChannel(channel);
+        Notification.Builder notification = new Notification.Builder(this, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(text)
+                .setSmallIcon(R.drawable.ic_cassette_tape_svgrepo_com_1)
+                .setAutoCancel(true);
+        NotificationManagerCompat.from(this).notify(1, notification.build());
+        super.onMessageReceived(remoteMessage);
+    }
+
+}
