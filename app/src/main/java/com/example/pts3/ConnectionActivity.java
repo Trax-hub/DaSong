a926ddf079ea840912a333e5aeda768655ad7a6e
Pablo <pablo.foucher1@gmail.com>
2021-10-14 08:59:34 +0200

    connection

---
+++ app/src/main/java/com/example/pts3/ConnectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.pts3;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ConnectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connection);
+    }
+}
