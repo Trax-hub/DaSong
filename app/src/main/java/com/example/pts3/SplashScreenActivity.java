e358253e7d247871f86d6b710052e828e534d8c2
alexis <avnl.alexis@gmail.com>
2021-11-08 08:51:57 +0100

    splash screen

---
+++ app/src/main/java/com/example/pts3/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.example.pts3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 5000; //splash screen will be shown for 2 seconds
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent(SplashScreenActivity.this, HomeActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
+
