ebdb0faee30f1556632af6e46c893aa5af2e58ac
jcholet <jonas.cholet@hotmail.fr>
2022-01-05 19:48:07 +0100

    profile pic maybe

--- app/src/main/java/com/example/pts3/ProfileActivity.java
+++ app/src/main/java/com/example/pts3/ProfileActivity.java
@@ -1,24 +1,38 @@
 package com.example.pts3;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.net.URI;
+
 public class ProfileActivity extends AppCompatActivity{
+
+    static final int REQUEST_IMAGE_GET = 1;
+
     private ListView friendList;
     private ImageView signOut;
     private Button goToFav;
     private Button findFriends;
     private TextView pseudo;
+    private ImageView profilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +44,8 @@ public class ProfileActivity extends AppCompatActivity{
         signOut = findViewById(R.id.signOut);
         goToFav = (Button) findViewById(R.id.goToFav);
         pseudo = findViewById(R.id.pseudo);
+        profilePic = findViewById(R.id.profilePic);
+        URI picURI = null;
 
         FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
         pseudo.setText(currentUser.getDisplayName());
@@ -39,6 +55,13 @@ public class ProfileActivity extends AppCompatActivity{
             public void onClick(View view) {
                 finish();
                 startActivity(new Intent(ProfileActivity.this, FavoriteActivity.class));
+            }
+        });
+
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageChooser();
             }
         });
 
@@ -58,6 +81,26 @@ public class ProfileActivity extends AppCompatActivity{
                 startActivity(new Intent(ProfileActivity.this, FindFriendsActivity.class));
             }
         });
+    }
+
+    private void imageChooser() {
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), REQUEST_IMAGE_GET);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_IMAGE_GET) {
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    profilePic.setImageURI(selectedImageUri);
+                }
+            }
+        }
     }
 
 }
